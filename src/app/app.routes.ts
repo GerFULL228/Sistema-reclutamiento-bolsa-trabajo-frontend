14655b47258bad9587c368539100f750ceaccf56
Gerson <gersonmachuca24@gmail.com>
2026-06-25 00:34:42 -0500

    feat: estructura inicial de carpetas y configuracion de tailwindcss y primeng

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+
+    {
+        path: '',
+        loadChildren: () => import('../app/layout/public-layout/public-layout.route').then(p=>p.PUBLIC_LAYOUT_ROUTE)
+    }
+];
---
+++ src/app/layout/public-layout/public-layout.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Header } from '../../shared/components/ui/header/header';
+import { Footer } from '../../shared/components/ui/footer/footer';
+
+
+@Component({
+  selector: 'app-public-layout',
+  imports: [RouterOutlet,Header,Footer],
+  templateUrl: './public-layout.html',
+  styleUrl: './public-layout.css',
+})
+export class PublicLayout {
+
+}
