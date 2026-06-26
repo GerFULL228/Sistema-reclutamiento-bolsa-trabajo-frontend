1b6a5965f48d6889349125694076d120c4173567
Gerson <gersonmachuca24@gmail.com>
2026-06-25 21:53:41 -0500

    feat: implementacion de todos los metodos para obtener el token permisos, roles, redirigir en tokenService

---
+++ src/app/core/services/token/token.ts
@@ -0,0 +1,88 @@
+
+import { Injectable, signal } from '@angular/core';
+import { jwtDecode } from "jwt-decode";
+@Injectable({
+  providedIn: 'root',
+})
+export class TokenService {
+
+  private accessToken = 'access_token';
+  private refreshToken = 'refresh_token';
+
+
+
+
+  permisos = signal<string[]>([]);
+
+    constructor(){
+
+      this.permisos.set(this.getPermisos());
+    }
+
+  initPermisos(){
+    this.permisos.set(this.getPermisos());
+
+  }
+
+  saveAccessToken(token: string) {
+    localStorage.setItem(this.accessToken, token)
+  }
+
+
+  getAccessToken(): string | null {
+    return localStorage.getItem(this.accessToken)
+  }
+
+  saveRefreshToken(token: string) {
+    localStorage.setItem(this.refreshToken, token);
+  }
+
+  getRefreshToken(): string | null {
+    return localStorage.getItem(this.refreshToken);
+  }
+
+  clearTokens() {
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('refresh_token');
+    this.permisos.set([]);
+  }
+
+  isLoggead(): boolean {
+    return !!this.getAccessToken();
+  }
+
+  private getDecodeToken(): any | null {
+    try {
+
+      const token = this.getAccessToken();
+
+      if (!token || token.split('.').length !== 3) {
+        return null;
+      }
+
+      return jwtDecode(token);
+    } catch {
+      return null
+    }
+  }
+
+  getRoles(): string {
+    return this.getDecodeToken()?.roles || [];
+  }
+
+  getPermisos(): string[] {
+    return this.getDecodeToken()?.permisos || [];
+  }
+
+  getHomeByRole(): string {
+    const roles = this.getRoles();
+
+    if (roles.includes('ROLE_admin')) return '/dashboard';
+    if (roles.includes('ROLE_barbero')) return '/dashboard';
+    if (roles.includes('ROLE_cliente')) return '/app';
+
+    return '/dashboard';
+  }
+}
+
+
