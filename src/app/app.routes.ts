371514e9d04b603658eb2c069099467908b34dfd
yas-ce <u23221782@utp.edu.pe>
2026-07-01 12:45:20 -0500

    feat(companies): crear vistas de perfil y edición de empresa

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { CompanyProfile } from './features/companies/pages/company-profile/company-profile';
+import { EditCompany } from './features/companies/pages/edit-company/edit-company';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'company-profile',
+    pathMatch: 'full'
+  },
+  {
+    path: 'company-profile',
+    component: CompanyProfile
+  },
+  {
+    path: 'edit-company',
+    component: EditCompany
+  }
+];
---
+++ src/app/features/companies/pages/company-profile/company-profile.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-company-profile',
+  standalone: true,
+  imports: [],
+  templateUrl: './company-profile.html',
+  styleUrl: './company-profile.scss',
+})
+export class CompanyProfile {
+
+}
